3c3492977d1f23e145ca645423887ee68ae60ffc
juancaruizc <juancruizcruz@gmail.com>
2021-03-12 09:04:29 -0700

    setup index.js and sever.js

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,8 @@
 // Write your "actions" router here!
+const express = require('express');
+
+const router = express.Router();
+
+const Action = require('./actions-model');
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -4,4 +4,15 @@ const server = express();
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
 
+const actionsRouter = require('./actions/actions-router');
+const projectsRouter = require('./projects/projects-router');
+
+server.use(express.json());
+server.use('/api/actions', actionsRouter);
+server.use('/api/projects', projectsRouter);
+
+server.get('/', (req, res) => {
+  res.send('<h1>Welcome to Mars</h1>');
+});
+
 module.exports = server;
