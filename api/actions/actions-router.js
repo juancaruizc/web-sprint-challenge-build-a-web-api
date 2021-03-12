e38a17ed2218e4f64596841976b6691211c9f28a
juancaruizc <juancruizcruz@gmail.com>
2021-03-12 09:35:15 -0700

    mvp met

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -5,4 +5,61 @@ const router = express.Router();
 
 const Action = require('./actions-model');
 
+router.get('/', (req, res) => {
+  Action.get()
+    .then((action) => {
+      res.status(200).json(action);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ message: `Error fetching the actions ${err.message}` });
+    });
+});
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  Action.get(id)
+    .then((action) => {
+      res.status(200).json(action);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ message: `Error getting the action ${err.message}` });
+    });
+});
+router.post('/', (req, res) => {
+  const action = req.body;
+
+  Action.insert(action)
+    .then((action) => {
+      res.status(201).json(action);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: `Error adding action ${err.message}` });
+    });
+});
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  Action.update(id, changes)
+    .then((updatedAction) => {
+      res.status(200).json(updatedAction);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: `Error updating action ${err.message}` });
+    });
+});
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  Action.remove(id)
+    .then((deletedAction) => {
+      res.status(200).json(deletedAction);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: `Error deleting action ${err.message}` });
+    });
+});
+
 module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -5,4 +5,80 @@ const router = express.Router();
 
 const Project = require('./projects-model');
 
+router.get('/', (req, res) => {
+  Project.get()
+    .then((projects) => {
+      res.status(200).json(projects);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ message: `Error getting projects ${err.message}` });
+    });
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  Project.get(id)
+    .then((project) => {
+      res.status(200).json(project);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ message: `Error getting projects ${err.message}` });
+    });
+});
+
+router.post('/', (req, res) => {
+  const project = req.body;
+  Project.insert(project)
+    .then((newProject) => {
+      res.status(201).json(newProject);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: `Error adding projects ${err.message}` });
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  Project.update(id, changes)
+    .then((updatedProject) => {
+      res.status(200).json(updatedProject);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ message: `Error updating projects ${err.message}` });
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  Project.remove(id)
+    .then((deletedProject) => {
+      res.status(200).json(deletedProject);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ message: `Error deleting projects ${err.message}` });
+    });
+});
+
+router.get('/:id/actions', (req, res) => {
+  const { id } = req.params;
+  Project.getProjectActions(id)
+    .then((id) => {
+      res.status(200).json(id);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ message: `Error getting projects actions ${err.message}` });
+    });
+});
+
 module.exports = router;
